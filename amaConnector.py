deb17dc4a1a73c77024d24a9b734c63bb5cddb3c
christian.demmler <christian>
2023-03-21 16:31:24 +0100

    First commit

---
+++ amaConnector.py
@@ -0,0 +1,208 @@
+import psycopg2
+
+import pandas.io.sql as sqlio
+from shapely import wkt, wkb
+import geopandas
+import pandas
+import os
+import datetime
+class amaAccess:
+    access = []
+
+    def __init__(self, inputfile, sep=':'):
+        try:
+            self.access = pandas.read_csv(inputfile,sep=sep)
+        except:
+            print("=========Error======")
+            print("You most likely don't have the configuration file configured out.")
+            print ("Please paste or create a configuration file into location '%s'"%(inputfile))
+            print("This is a simple CSV (comma separated) textfile")
+            print("make sure it uses the '%s' separator"%sep)
+            print("and has the following column names in the first line -in fact, just copy&paste that and fill in the correct values in the second line, or ask the DB guy for a prepped file:")
+            print()
+            print('paste into %s and modify:'%inputfile)
+            print('==================')
+            print(sep.join(['host','db','port','user','password']))
+            print(sep.join(['example.ama.host','example_ama_db','12345','your_username','super secret!!!']))
+            print("==================")
+    def insertQuery(self, schema, table, df):
+        columns = ','.join('"'+df.columns+'"')
+
+        server_ip = self.access['host'][0]
+        db_name = self.access['db'][0]
+        username = self.access['user'][0]
+        pwd = self.access['password'][0]
+        port = self.access['port'][0]
+        # ...and connect please
+        connstr = "host='{}' port={} dbname='{}' user={} password='{}'".format(server_ip, port, db_name, username,
+                                                                               pwd)
+        conn = psycopg2.connect(connstr
+                                )
+
+        cursor = conn.cursor()
+        insert_req = []
+        for index, data in df.iterrows():
+            values=[]
+            for col in data:
+                s = str(col)
+                if (s == 'None') or (s=='nan'):
+                    s = 'NULL'
+                if (type(col) == int or type(col)== float or s=='NULL' or s=='True' or s=='False'):
+                    values.append(r'%s'%s)
+                else:
+                    values.append(r"'%s'" % s)
+            strval = ','.join(values)
+            insert_req.append("INSERT into %s.%s(%s) values(%s) ON CONFLICT DO NOTHING" % (schema, table, columns, strval))
+
+        try:
+            for el in insert_req:
+                print(el)
+                cursor.execute(el)
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cursor.close()
+            return 1
+        cursor.close()
+
+
+    def query(self,query_list='SELECT * FROM event_full', constraint=''):
+        try:
+            if (query_list.lower().find('from') != -1):
+                # Set up our connection
+                #access = getAccess(r'c:\temp\ama_out\access.txt')
+                server_ip = self.access['host'][0]
+                db_name = self.access['db'][0]
+                username = self.access['user'][0]
+                pwd = self.access['password'][0]
+                port = self.access['port'][0]
+                # ...and connect please
+                connstr = "host='{}' port={} dbname='{}' user={} password='{}'".format(server_ip, port, db_name, username, pwd)
+                conn = psycopg2.connect(connstr
+                    )
+                sql = '{} {};'.format(query_list.strip(), constraint.strip())
+
+
+                dat = sqlio.read_sql_query(sql, conn)
+                conn = None
+                return dat
+
+            else:
+                print('Something wrong with the query. No "select" and/or "from" encountered.')
+        except:
+            print('Error: Connection error.')
+            raise
+
+access =[]
+
+def getAccess(inputfile, sep=';'):
+    access = pandas.read_csv(inputfile,sep=sep)
+    return access
+
+
+
+def query(query_list='SELECT * FROM event_full', constraint=''):
+    try:
+        if (query_list.lower().find('from') != -1):
+            # Set up our connection
+            #access = getAccess(r'c:\temp\ama_out\access.txt')
+            server_ip = access['host'][0]
+            db_name = access['db'][0]
+            username = access['user'][0]
+            pwd = access['password'][0]
+            port = access['port'][0]
+            # ...and connect please
+            connstr = "host='{}' port={} dbname='{}' user={} password='{}'".format(server_ip, port, db_name, username, pwd)
+            conn = psycopg2.connect(connstr
+                )
+            sql = '{} {};'.format(query_list.strip(), constraint.strip())
+
+
+            dat = sqlio.read_sql_query(sql, conn)
+            conn = None
+            return dat
+
+        else:
+            print('Something wrong with the query. No "select" and/or "from" encountered.')
+    except:
+        print('Error: Connection error.')
+        raise
+
+
+# Connect to an existing database
+def eventlist(constraint='', geom_only=False):
+    if (geom_only):
+        sql = 'SELECT * FROM event_geom'
+    else:
+        sql = 'SELECT * FROM event_full'
+    dat = query(sql, constraint)
+    # drop connection after use
+
+    # use well-known-text geometry, create designated geometry column for geopandas
+    dat['geom'] = dat['pt'].apply(wkt.loads)
+
+    if dat is not None:
+        geodat = geopandas.GeoDataFrame(dat, geometry='geom')
+    return geodat
+
+def eventlist(constraint='', geom_only=False):
+    sql = 'SELECT * FROM event_full'
+    dat = query(sql, constraint)
+    # drop connection after use
+
+    # use well-known-text geometry, create designated geometry column for geopandas
+    dat['geom'] = dat['pt'].apply(wkt.loads)
+
+    if dat is not None:
+        geodat = geopandas.GeoDataFrame(dat, geometry='geom')
+    return geodat
+
+def designlist(constraint='', geom_only=False):
+
+    sql = 'SELECT * FROM event_full2 where not (geom_rel_event_poly3d is  NULL)'
+    dat = query(sql, constraint)
+    # drop connection after use
+
+    # use well-known-text geometry, create designated geometry column for geopandas
+    dat['geom'] = dat['geom_rel_event_poly3d'].apply(wkb.loads, hex=True)
+
+    if dat is not None:
+        geodat = geopandas.GeoDataFrame(dat, geometry='geom')
+    return geodat
+
+
+# Connect to an existing database
+def pathlist(constraint=''):
+
+    sql = 'SELECT * FROM event_full2'
+    dat = query(sql, constraint)
+    # drop connection after use
+
+    # use well-known-text geometry, create designated geometry column for geopandas
+    dat['geom'] = dat['path_ln'].apply(wkb.loads,hex=True)
+
+    if dat is not None:
+        geodat = geopandas.GeoDataFrame(dat, geometry='geom')
+        geodat.to_csv(filePath, sep=',', float_format='%.')
+    return geodat
+
+
+def event_minimum(constraint='', use3d=True):
+    # will return only event point and path line geometries (either 3d oder 2d) from events as selected by the constraint list
+    if (use3d):
+        query_list = 'SELECT pt3d, path_ln3d FROM event_full'
+        geom_col = 'pt3d'
+    else:
+        query_list = 'SELECT pt, path_ln FROM event_full'
+        geom_col = 'pt'
+    dat = query(query_list, constraint)
+
+    dat['geom'] = dat[geom_col].apply(wkt.loads)
+    if dat is not None:
+        geodat = geopandas.GeoDataFrame(dat, geometry='geom')
+    return geodat
+
+
+
+
---
+++ avaframeExport.py
@@ -0,0 +1,168 @@
+import amaConnector
+import os
+from os import path
+import geopandas, pandas as pd
+import gdal
+from shapely import wkb
+from datetime import datetime
+def checkDir(dir):
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+    return dir
+
+def checkPath(filePath):
+    filePath = filePath.replace('/',path.sep).replace('\\',path.sep)
+    #use system specific path separators
+    dir = checkDir(path.dirname(filePath))
+    #prepare directory if not yet exists
+    outPath = path.join(dir, path.basename(filePath))
+    if path.isfile(outPath):
+        try:
+            os.remove(outPath)
+        except:
+            print('cannot remove file, probably trying to overwrite a file we just created.')
+            outPath=''
+
+    return outPath
+def grabRaster(config, outdir, design_event=False, constraint=''):
+    if (design_event):
+        schema = 'design' # Design refers to reference avalanches meant for designing mitigation measures etc.
+    else:
+        schema = 'public' # Public refers to actual, real-life avalanches
+    projstr = amaConnect.query("select * from getclosestconf('%s','projection_raster')"%config)['getclosestconf'][0]
+    #getting the output projection config as stored in the config table
+
+    epsg = int(projstr.lower().replace('epsg:','').strip())
+    print('using output projection EPSG:%d'%epsg)
+    buffersize = int(amaConnect.query("select * from getclosestconf('%s','raster_buffer')"%config)['getclosestconf'][0])
+    print('using a buffer size of %d [coordinate system units]'%buffersize)
+    outputformat = amaConnect.query("select * from getclosestconf('%s','raster_format')"%config)['getclosestconf'][0]
+    #
+    pathList = amaConnect.query('select distinct path_id, event_id from %s.event_full' % schema, constraint)
+    for index, path in pathList.iterrows():
+        path_id= path['path_id']
+        event_id = path['event_id']
+        #rasterquery = "with extent as (select st_swapordinates(st_buffer(st_transform(ln,%s),%d),'xy') as ext from paths where paths.path_id = %d) \
+        #                            select st_astiff(st_union(st_clip(rast, ext))) as raster from dem right join extent on st_intersects(dem.rast, extent.ext) group by ext" % (
+        #epsg, buffersize, path_id)
+        rast = amaConnect.query("select * from st_astiff(getraster(%d,%d,%d))"%(path_id, epsg, buffersize))
+        outfile = checkPath(amaConnect.query("select * from getstructure(%d,'raster_dem','%s')"%(event_id,config))['getstructure'][0].replace('%outdir%',outdir))
+        if len(outfile)>0:
+            print('saving tiff to %s'%outfile)
+            with  open(outfile, 'wb') as savefile:
+                savefile.write(rast['st_astiff'][0])
+            if outputformat.lstrip(' .').lower() not in ['tif', 'tiff', 'geotiff']:
+                print('DB configuration suggests using raster format "%s"'% outputformat)
+                customOut = checkPath(outfile.replace('.tif', '.%s' % outputformat.lstrip(' .').lower()))
+                print('Writing output file %s'%customOut)
+                ds = gdal.Open(outfile)
+                gdal.Translate(customOut, ds)
+                ds = None
+                if os.path.isfile(customOut):
+                    print('file converted to selected output format, removing tiff %s'%outfile)
+                    try:
+                        os.remove(outfile)
+                    except:
+                        print('cannot remove file right now. you might have to live with a bit of additional hdd clutter for now, remove it yourself if you like')
+        else:
+            print('skipping.')
+    return len(pathList)
+
+
+def grabEvents(config, outpath,design_event=False, constraint = '' ):
+    if (design_event):
+        schema = 'design' # Design refers to reference avalanches meant for designing mitigation measures etc.
+    else:
+        schema = 'public' # Public refers to actual, real-life avalanches
+    projstr = amaConnect.query("select * from getclosestconf('%s','projection_raster')" % config)['getclosestconf'][0]
+    # getting the output projection config as stored in the config table
+
+    epsg = int(projstr.lower().replace('epsg:', '').strip())
+    print('using output projection EPSG:%d' % epsg)
+    eventList = amaConnect.query('select distinct event_id from %s.event_full'%schema, constraint)
+    #this retrieves the event_ids of all relevant events
+
+
+    geometryCols = amaConnect.query("select * from getclosestconf('%s','exportgeometry')"%config)
+    #this asks the database for the stored geometry column names which should get exported in the current configuration
+
+    for index, event in eventList.iterrows():
+        event_id = event['event_id']
+        print('exporting event_id %d'%event_id)
+        for geomCol in geometryCols['getclosestconf'][0].split(','):
+            col = geomCol.strip()
+            print('collecting data for geometry column %s' % col)
+            geometry = amaConnect.query("select * from %s.exporteventgeom(%d, '%s', '%s')"%(schema, event_id, col, config))
+            #this retrieves the geometry in combination with an output path according to the current config - structure_geom_xyz
+
+
+            if not (geometry['geom'].isnull()[0]): #only work with existing geometries
+                geometry['geom'] = geometry['geom'].apply(wkb.loads, hex=True)
+
+                attr = amaConnect.query(
+                    "select * from %s.extractattributes(%d, '%s', '%s')" % (schema, event_id, col, config))
+                #this retrieves the selected set of attributes according to the current configuration and the current geometry column
+                # one may set different (or none at all) attributes to be included in paths, release lines etc.
+                attributes = pd.DataFrame([attr['value']]).rename(columns=attr['key']).reset_index(drop=True)
+                feature = geometry.join(attributes)
+                geodf =  geopandas.GeoDataFrame(feature, geometry='geom').set_crs(projstr)
+
+                outPath = geodf['struct'][0].replace('%outdir%',outpath)
+                #filling in the last variable, %outdir%, in the output path
+                outPath=checkPath(outPath)
+                if len(outPath)>0:
+                #this will prepare the subdirectory and change the output path if necessary; general aim is to get a valid, writeable path within an existing directory
+                    print('using output path %s'%outPath)
+                    geodf.to_file(outPath, driver='ESRI Shapefile')
+                else:
+                    print('skipping file')
+            else:
+                print('Geometry empty, skipping output!')
+            #exportfile(dataOut, geometry['struct'], outpath)
+
+    return len(eventList)
+
+
+global amaConnect
+outdir = ''
+#outdir = r'c:\temp\my_export_dir'
+#if you want to, uncomment and set your own export dir above -this should point to an already created directory.
+#otherwise, the following function will create a valid, individual export directory based on the current time:
+exportdate = datetime.now().strftime('export_%Y-%m-%d_%H-%M-%S')
+if not os.path.isdir(outdir):
+    outdir = path.join(os.path.dirname(os.getcwd()),exportdate)
+    print('Using output directory %s'%outdir)
+    os.mkdir(outdir)
+
+
+#fill in configurations if necessary:
+configuration = 'avaframe' # this refers to the configuration variables, as set in the DB table "ext_project" ("External Project configuration" in QGIS)
+useDesignEvents = False #set to True if you want to export Design Events instead of real recorded Events
+constraint = "where not(st_isempty(geom_rel_event_ln) or st_isempty(geom_event_ln))" #this constraint delivers all avalanches that have filled out release event line and event deposition line geometries.
+
+
+
+accessFile = path.join(os.getcwd(),'access.txt')
+
+
+amaConnect = amaConnector.amaAccess(accessFile)
+try:
+    with open(path.join(outdir,'log.txt'),'w') as file:
+        file.write('Time: %s\n'%exportdate)
+        file.write('Exporting db using export configuration "%s"\n'%configuration)
+        file.write('Design Events: %s\n' %useDesignEvents)
+        file.write('Using constraint: "%s"\n'%constraint)
+
+except:
+    print ("ERROR --------- no writing access to directory '%s'!"%outdir)
+#initAma(outpath, accessFile, ':')
+eventsRes= grabEvents(configuration,outdir, useDesignEvents,constraint)
+rasterRes= grabRaster(configuration,outdir, useDesignEvents,constraint)
+try:
+    with open(path.join(outdir,'log.txt'),'a') as file:
+        file.write('====Results====\n')
+        file.write('Written %d events"\n'%eventsRes)
+        file.write('Written %d path rasters\n'%rasterRes)
+
+except:
+    print ("ERROR --------- no writing access to directory '%s'!"%outdir)
